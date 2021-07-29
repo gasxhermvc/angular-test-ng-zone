057b53b4c006a4309d1aba24b54ec8b7eebcf290
Tanasit Auewongaree <006006@cdg.co.th>
2021-07-29 20:01:53 +0700

    update readme

--- README.md
+++ README.md
@@ -1,27 +1,31 @@
-# NgZoneTest
+## ngZone
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.1.3.
+เครื่องมือสำหรับจัดการ changeDetectiong ในส่วนของ Asynchonous เช่น เมื่อต้องการกำหนดการอัพเดตหน้าเว็บแบบชัวร์ ๆ สามารถกำหนดได้ 2 รูปแบบหลักๆ คือ auto และ manual
 
-## Development server
+```typescript
+//html
+{{ data }}
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
+//typescript
+data:any = "inital value"
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+constructor(private cdr: ChangeDetectionRef){}
 
-## Build
+ngOnInit(){
+  //Auto
+  this.ngZone.run(() => {
+    setTimeout(() => {
+      this.data = "test"
+    },0)
+  }
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+  //Manual
+  this.ngZone.runOutsideAngular(() => {
+    setTimeout(() => {
+      this.data = "test"
+      this.cdr.detectChanges();
+    },0)
+  });
+}
+```
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  NgZone,
+} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+declare let Zone: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'ng-zone-test';
+  data: any = 'initial value';
+  serverUrl = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(
+    private httpClient: HttpClient,
+    private ngZone: NgZone,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  // ngOnInit() {
+  //   this.httpClient.get(this.serverUrl).subscribe((response: any) => {
+  //     // user does not need to trigger change detection manually
+  //     this.data = response.data;
+  //   });
+  // }
+
+  // ngOnInit() {
+  //   setTimeout(() => {
+  //     // user does not need to trigger change detection manually
+  //     this.data = 'value updated';
+  //   });
+  // }
+
+  // ngOnInit() {
+  //   Promise.resolve(1).then((v) => {
+  //     // user does not need to trigger change detection manually
+  //     this.data = v;
+  //   });
+  // }
+
+  ngOnInit() {
+    // You know no data will be updated,
+    // so you don't want to trigger change detection in this
+    // specified operation. Instead, call ngZone.runOutsideAngular()
+    this.ngZone.runOutsideAngular(() => {
+      setTimeout(() => {
+        // update component data
+        // but don't trigger change detection.
+        this.data = 'updated value';
+      });
+    });
+
+    this.ngZone.runOutsideAngular(() => {
+      this.ngZone.run(() => {
+        this.httpClient.get(this.serverUrl).subscribe((response: any) => {
+          console.log(response);
+          // user does not need to trigger change detection manually
+          this.data = { ...response };
+          this.cdr.markForCheck();
+        });
+      });
+
+      // this.data = { h: 'hello' };
+      setTimeout(() => {
+        this.data = { h: 'hello' };
+        this.cdr.detectChanges();
+      }, 500);
+    });
+
+    Promise.resolve(1).then((v) => {
+      // user does not need to trigger change detection manually
+      this.data = v;
+    });
+  }
 }
